b0713294e339d6c35c8cef5f1f9eaf59c927ed81
andrehenrique1986 <andrehenriquelima1986@gmail.com>
2026-09-23 16:43:57 -0300

    Criando o Dockerfile

---
+++ frontend-react/frontend/Domain/ProcureToPay.Domain/Entities/Fornecedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcureToPay.Domain.Entities
+{
+    public class Fornecedor
+    {
+        public int FornecedorId { get; private set; }
+        public string FornecedorNome { get; private set; }
+        public string? FornecedorNomeFantasia { get; private set; }
+        public string FornecedorCpfCnpj {  get; private set; }  
+    }
+}
---
+++ frontend-react/frontend/Domain/ProcureToPay.Domain/Entities/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcureToPay.Domain.Entities
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; private set; }
+        public string UsuarioNome { get; private set; }
+        public string UsuarioEmail { get; private set; }
+        public string UsuarioCPF { get; private set; }
+        public string UsuarioSenhaHash { get; private set; }
+
+    }
+}
